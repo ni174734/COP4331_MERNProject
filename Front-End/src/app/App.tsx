3c8829d6d72bce0bd3360e0b1e06c3b3cd6a00a7
LincolnHuber <162394852+LincolnHuber@users.noreply.github.com>
2026-04-03 17:52:23 -0400

    Update App.tsx

--- Front-End/src/app/App.tsx
+++ Front-End/src/app/App.tsx
@@ -1,6 +1,10 @@
-import { RouterProvider } from 'react-router';
-import { router } from './routes';
+import { Store } from './components/ui/Store';
+import '../styles/index.css';
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <div className="min-h-screen bg-slate-950">
+      <Store />
+    </div>
+  );
 }
