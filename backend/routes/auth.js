5e71d682032ebe0ec6341a6d9a53a45eb9251bee
LincolnHuber <162394852+LincolnHuber@users.noreply.github.com>
2026-04-10 13:51:45 -0500

    changed /models to /Models

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express        = require('express');
 const bcrypt         = require('bcryptjs');
 const jwt            = require('jsonwebtoken');
-const User           = require('../models/users');
+const User           = require('../Models/users');
 const sendVerificationEmail = require('../utils/sendEmail');
 
 const router = express.Router();
